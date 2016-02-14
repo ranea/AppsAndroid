6e5121dc7733f7183f61b806d4c31faadfe06b2b
Antonio Álvarez Caballero <analca3@gmail.com>
2016-02-14 21:49:50 +0100

    Se ven cosas! Gracias @Griger y @LuisSuall !

--- PuntoGPSQR/app/src/main/java/practica3/npi/puntogpsqr/NavegacionActivity.java
+++ PuntoGPSQR/app/src/main/java/practica3/npi/puntogpsqr/NavegacionActivity.java
@@ -1,72 +1,227 @@
 package practica3.npi.puntogpsqr;
 
-import android.content.Context;
+
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.location.Location;
-import android.net.Uri;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.location.Location;
 
+import android.widget.TextView;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+import android.util.Log;
+
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationListener;
+
+
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLng;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
-import fr.quentinklein.slt.LocationTracker;
-import fr.quentinklein.slt.TrackerSettings;
-
-public class NavegacionActivity extends AppCompatActivity {
+public class NavegacionActivity extends FragmentActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
     // TODO change name
     private static final int INTENT_V1 = 1;
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+    private LocationRequest mLocationRequest;
+    private String mLastUpdateTime;
+
+    private GoogleMap mapa;
+    private ArrayList<Location> mListLocations;
+
+    private boolean mRequestingLocationUpdates = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navegacion);
 
-        LocationTracker tracker = new LocationTracker(getApplicationContext()) {
-            @Override
-            public void onTimeout() {
-                Toast.makeText(getApplicationContext(), "No position found", Toast.LENGTH_LONG).show();
-            }
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
-            @Override
-            public void onLocationFound(Location location) {
-                Toast.makeText(getApplicationContext(), location.toString(), Toast.LENGTH_LONG).show();
-            }
-        };
-        tracker.startListening();
+        if (mLocationRequest == null) {
+            createLocationRequest();
+        }
 
-        // *********************************************************************************
+        mapa = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapa)).getMap();
+        mListLocations = new ArrayList<>();
 
-        //Intent intent = getIntent();
-        //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        //Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+//        updateValuesFromBundle(savedInstanceState);
 
-        // *********************************************************************************
-        // Create a Uri from an intent string. Use the result to create an Intent.
-        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
-        // Make the Intent explicit by setting the Google Maps package
-        //Uri gmmIntentUri = Uri.parse("google.navigation:q=37.19678168548899,-3.62465459523194 &mode=w");
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=37.1809053,-3.6133 &mode=w");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
 
-        // Attempt to start an activity that can handle the Intent
-        startActivityForResult(mapIntent, INTENT_V1);
+    }
+
+//    private void updateValuesFromBundle(Bundle savedInstanceState) {
+//        if (savedInstanceState != null) {
+//            // Update the value of mRequestingLocationUpdates from the Bundle, and
+//            // make sure that the Start Updates and Stop Updates buttons are
+//            // correctly enabled or disabled.
+//            if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+//                mRequestingLocationUpdates = savedInstanceState.getBoolean(
+//                        REQUESTING_LOCATION_UPDATES_KEY);
+//                setButtonsEnabledState();
+//            }
+//
+//            // Update the value of mCurrentLocation from the Bundle and update the
+//            // UI to show the correct latitude and longitude.
+//            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+//                // Since LOCATION_KEY was found in the Bundle, we can be sure that
+//                // mCurrentLocationis not null.
+//                mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+//            }
+//
+//            // Update the value of mLastUpdateTime from the Bundle and update the UI.
+//            if (savedInstanceState.keySet().contains(LAST_UPDATED_TIME_STRING_KEY)) {
+//                mLastUpdateTime = savedInstanceState.getString(
+//                        LAST_UPDATED_TIME_STRING_KEY);
+//            }
+//            updateUI();
+//        }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+//        Location currentPos = LocationServices.​FusedLocationApi.getLastLocation(​mGoogleApiClient​​);
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+
+    }
+
+    @Override
+    public void onConnectionFailed (ConnectionResult result) {
+
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == INTENT_V1) {
-            Toast.makeText(this, "NavegacionActivity: " + getDateTime(), Toast.LENGTH_LONG).show();
-        }
+
     }
+
+//    public void onSaveInstanceState(Bundle savedInstanceState) {
+//        savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+//                mRequestingLocationUpdates);
+//        savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+//        savedInstanceState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdateTime);
+//        super.onSaveInstanceState(savedInstanceState);
+//    }
 
     private String getDateTime() {
         // get date time in custom format
         SimpleDateFormat sdf = new SimpleDateFormat("[yyyy/MM/dd - HH:mm:ss]");
         return sdf.format(new Date());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        mListLocations.add(mCurrentLocation);
+        updateUI();
+    }
+
+//    @Override
+//    public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderEnabled(String provider) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderDisabled(String provider) {
+//
+//    }
+
+    public void updateUI() {
+        if (mCurrentLocation != null) {
+            LatLng tmp = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+//            TextView mLatitudeText = (TextView) findViewById(R.id.mLatitudeText);
+//            mLatitudeText.setText(String.valueOf(mCurrentLocation.getLatitude()));
+//            TextView mLongitudeText = (TextView) findViewById(R.id.mLongitudeText);
+//            mLongitudeText.setText(String.valueOf(mCurrentLocation.getLongitude()));
+//
+//            TextView mLastUpdateTimeTextView = (TextView) findViewById(R.id.mLastUpdateTimeTextView);
+//            mLastUpdateTimeTextView.setText(mLastUpdateTime);
+
+            Log.w("GPSQR", "Añadiendo localización");
+            mapa.addMarker(new MarkerOptions().title("Punto ").position(tmp));
+        }
+        else
+            Log.w("GPSQR", "No hay localización!");
     }
 }
